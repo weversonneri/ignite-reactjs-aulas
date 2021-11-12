11612a5b1227c2986dbdb690496cbdb4a5a7814d
weversonneri <weversonneri@outlook.com>
2021-11-12 12:57:28 -0300

    :lipstick: global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -44,6 +44,16 @@ export const GlobalStyle = styled.createGlobalStyle`
     font-weight: 400;
   }
 
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }

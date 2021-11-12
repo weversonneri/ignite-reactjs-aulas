334465af6d4c7c56a810a56b544559b276a152f3
weversonneri <weversonneri@outlook.com>
2021-11-12 19:54:53 -0300

    :clown_face: mock api
    using miragejs and axios to mock api to frontend

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transaction', () => [
+      {
+        id: 1,
+        title: 'Desenvolvimento de blog',
+        amount: 1200,
+        type: 'deposit',
+        category: 'Desenvolvimento',
+        date: new Date()
+      }
+    ]);
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>

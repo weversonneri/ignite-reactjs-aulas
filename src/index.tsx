d0d9b93f777eb432dcea495eed82926f4139cbac
weversonneri <weversonneri@outlook.com>
2021-11-14 01:12:00 -0300

    :seedling: create transactio post and seed

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de blog',
+          amount: 1200,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date('2021-10-10 10:00:00')
+        },
+        {
+          id: 2,
+          title: 'Compras',
+          amount: 200,
+          type: 'withdraw',
+          category: 'alimento',
+          createdAt: new Date('2021-09-10 13:30:00')
+        }
+      ]
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
-    this.get('/transaction', () => [
-      {
-        id: 1,
-        title: 'Desenvolvimento de blog',
-        amount: 1200,
-        type: 'deposit',
-        category: 'Desenvolvimento',
-        date: new Date()
-      }
-    ]);
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
   }
 });
 

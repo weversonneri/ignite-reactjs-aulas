1be91f61887031ffc7ed307f034e5b2a2991f721
weversonneri <weversonneri@outlook.com>
2022-01-07 00:40:15 -0300

    finish chapter III

--- 03-ig-news/src/pages/_app.tsx
+++ 03-ig-news/src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+import '../styles/global.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	return (
+		<NextAuthProvider session={session}>
+			<Header />
+			<Component {...pageProps} />
+			<ToastContainer />
+		</NextAuthProvider>
+	);
 }
 
 export default MyApp;
--- 03-ig-news/src/pages/index.tsx
+++ 03-ig-news/src/pages/index.tsx
@@ -1,5 +1,57 @@
-export default function Home() {
-  return (
-   <h1>Hello</h1>
-  )
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+import styles from './home.module.scss';
+
+interface HomeProps {
+	product: {
+		priceId: string;
+		amount: number;
+	};
 }
+
+export default function Home({ product }: HomeProps) {
+	return (
+		<>
+			<Head>
+				<title>Home | ig.news</title>
+			</Head>
+
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>👏🏽 Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world.
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span>for {product.amount} month</span>
+					</p>
+
+					<SubscribeButton priceId={product.priceId} />
+				</section>
+
+				<img src='/images/avatar.svg' alt='Girl coding' />
+			</main>
+		</>
+	);
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const price = await stripe.prices.retrieve('price_1K9KYdC5R6KLOfo5w30EtgXY');
+
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD',
+		}).format(price.unit_amount / 100),
+	};
+
+	return {
+		props: {
+			product,
+		},
+	};
+};

7156d666c6788d2df219c5ea9c8b0cd490de1993
Weverson Neri <weversonneri@outlook.com>
2022-06-19 12:35:24 -0300

    ...

--- 03-ig-news/src/pages/api/auth/[...nextauth].ts
+++ 03-ig-news/src/pages/api/auth/[...nextauth].ts
@@ -7,6 +7,7 @@ import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
 	// Configure one or more authentication providers
+	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID,
